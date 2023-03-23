09220c9c5dd7b48c038a5dc588e4b380f983d434
Alexander <atlexeide@gmail.com>
2023-03-23 20:12:16 +0100

    Update project links

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-let answer = parseInt(prompt("Please enter the number you'd like to FizzBuzz up to: "))
+let i = 10;
+while (i != 0) {
+    if (i === 10) { para.textContent = `Countdown ${i}`; }
+    else if (i <= 10 && i >= 0) { console.log(`${i}`); }
+    else if (i === 0) { console.log(`Blast off!`); }
+    else { console.log(`Error`;) }
+    i--
+}
