13c01ddcfd3b346a04d6fc705734647db8660f07
Alexander <atlexeide@gmail.com>
2023-03-27 11:40:22 +0200

    Redesign

--- test.js
+++
@@ -1,8 +0,0 @@
-let i = 10;
-while (i != 0) {
-    if (i === 10) { para.textContent = `Countdown ${i}`; }
-    else if (i <= 10 && i >= 0) { console.log(`${i}`); }
-    else if (i === 0) { console.log(`Blast off!`); }
-    else { console.log(`Error`;) }
-    i--
-}
