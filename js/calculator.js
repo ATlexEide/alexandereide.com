623f8f7a29b1d7b201fb1ecae8baf6621258ed46
Alexander Eide <atlexeide@gmail.com>
2023-09-01 07:47:49 +0200

    Update calculator logic

--- js/calculator.js
+++ js/calculator.js
@@ -1,5 +1,5 @@
 function add(num, num2) {
-    return `${num + num2}`;
+    return Number(num) + Number(num2);
 };
 
 function subtract(num, num2) {
@@ -14,20 +14,30 @@ function divide(num, num2) {
     return num / num2;
 };
 
-let firstNumber;
 let secondNumber;
 let operator;
+let firstNumber;
+const display = document.getElementById('display');
 
 function operate(firstNumber, operator, secondNumber) {
-    operator === '+' ? console.log(add(firstNumber, secondNumber)) :
-        operator === '-' ? console.log(subtract(firstNumber, secondNumber)) :
-            operator === '*' ? console.log(multiply(firstNumber, secondNumber)) :
-                operator === '/' ? console.log(divide(firstNumber, secondNumber)) : console.log('Error')
-    return
+    let result;
+    if (operator === '+') {
+        result = add(firstNumber, secondNumber);
+    } else if (operator === '-') {
+        result = subtract(firstNumber, secondNumber);
+    } else if (operator === '*') {
+        result = multiply(firstNumber, secondNumber);
+    } else if (operator === '/') {
+        result = divide(firstNumber, secondNumber);
+    } else {
+        result = 'Error';
+    }
+    display.textContent = result.toString();
+    return result;
 };
 
-const numberButtons = [];
-function test() {
+function writeToDisplay() {
+    display.textContent = '';
     let displayValue = '';
     for (let i = 0; i < 10; i++) {
         document.getElementById(`button${i}`).addEventListener('click', () => {
@@ -36,9 +46,50 @@ function test() {
             const display = document.getElementById('display').textContent = `${displayValue}`;
         })
     };
+    getFirstNumber();
 };
-//document.getElementById(`button${i}`).addEventListener('click', () => {
-//  displayValue += `button${i}`
-//console.log('hi')
-//};
-test()
+
+function getFirstNumber() {
+    let operators = document.getElementsByClassName('operatorButton');
+    for (let i = 0; i < operators.length; i++) {
+        operators[i].addEventListener('click', () => {
+            firstNumber = document.getElementById('display').textContent;
+            operator = operators[i].textContent.toString();
+            document.getElementById('display').textContent = `${operator}`;
+            writeToDisplay()
+            getSecondNumber();
+        });
+    };
+};
+
+function getSecondNumber() {
+    document.getElementById('enter').addEventListener('click', () => {
+        secondNumber = document.getElementById('display').textContent;
+        document.getElementById('display').textContent = `${operate(firstNumber, operator, secondNumber)}`;
+    })
+};
+
+function clearButton() {
+    document.getElementById('clearButton').addEventListener('click', () => {
+        clear()
+        writeToDisplay()
+        console.log('yippe')
+    })
+};
+
+function clear() {
+    firstNumber = ;
+    secondNumber = null;
+    operator = null;
+    displayValue = null;
+    console.log(firstNumber)
+    console.log(secondNumber)
+    console.log(operator)
+    console.log(displayValue)
+    //display.textContent = '';
+
+};
+
+
+writeToDisplay()
+clearButton()
