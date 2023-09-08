2a5cdace1c2ca124d90fdd4833b724624ed46abc
Alexander Eide <atlexeide@gmail.com>
2023-09-08 05:32:36 +0200

    Update design and misc js

--- js/calculator.js
+++ js/calculator.js
@@ -14,11 +14,6 @@ function divide(num, num2) {
     return num / num2;
 };
 
-let secondNumber;
-let operator;
-let firstNumber;
-const display = document.getElementById('display');
-
 function operate(firstNumber, operator, secondNumber) {
     let result;
     if (operator === '+') {
@@ -36,60 +31,75 @@ function operate(firstNumber, operator, secondNumber) {
     return result;
 };
 
-function writeToDisplay() {
-    display.textContent = '';
-    let displayValue = '';
-    for (let i = 0; i < 10; i++) {
-        document.getElementById(`button${i}`).addEventListener('click', () => {
-            let currentValue = document.getElementById(`button${i}`).textContent;
-            displayValue += currentValue.toString();
-            const display = document.getElementById('display').textContent = `${displayValue}`;
-        })
-    };
-    getFirstNumber();
-};
-
-function getFirstNumber() {
-    let operators = document.getElementsByClassName('operatorButton');
-    for (let i = 0; i < operators.length; i++) {
-        operators[i].addEventListener('click', () => {
-            firstNumber = document.getElementById('display').textContent;
-            operator = operators[i].textContent.toString();
-            document.getElementById('display').textContent = `${operator}`;
-            writeToDisplay()
-            getSecondNumber();
-        });
-    };
-};
-
-function getSecondNumber() {
-    document.getElementById('enter').addEventListener('click', () => {
-        secondNumber = document.getElementById('display').textContent;
-        document.getElementById('display').textContent = `${operate(firstNumber, operator, secondNumber)}`;
-    })
-};
-
-function clearButton() {
-    document.getElementById('clearButton').addEventListener('click', () => {
-        clear()
-        writeToDisplay()
-        console.log('yippe')
-    })
-};
+function convertArrayToString(array) {
+    array = array.join('');
+    return array;
+}
 
 function clear() {
-    firstNumber = null;
-    secondNumber = null;
-    operator = null;
-    displayValue = null;
-    console.log(firstNumber)
-    console.log(secondNumber)
-    console.log(operator)
-    console.log(displayValue)
-    //display.textContent = '';
-
+    displayValueArray = [];
+    display.textContent = '';
 };
 
+///////////////////////////////////////////////////////////////////////////////
 
-writeToDisplay()
-clearButton()
+const numberButtons = document.getElementsByClassName('numberButton');
+const operatorButtons = document.getElementsByClassName('operatorButton');
+const enterButton = document.getElementById('=');
+const clearButton = document.getElementById('clearButton');
+let digitArray = [];
+let numberArray = [];
+const calculatorDisplay = document.getElementById('display');
+
+// Push digits to an array for later use and display them on screen
+for (let i = 0; i < numberButtons.length; i++) {
+    numberButtons[i].addEventListener('click', () => {
+        digitArray.push(numberButtons[i].textContent);
+        let digitString = digitArray.join('');
+        calculatorDisplay.textContent = digitString;
+    })
+}
+
+// Pushes another number and the operator to array 
+for (let i = 0; i < operatorButtons.length; i++) {
+    operatorButtons[i].addEventListener('click', () => {
+        calculatorDisplay.textContent = operatorButtons[i].id;
+        numberObj = { number: digitArray.join(''), operator: operatorButtons[i].id };
+        numberArray.push(numberObj)
+        digitArray = [];
+
+    })
+};
+
+// Finalizes the operation
+enterButton.addEventListener('click', () => {
+    numberObj = { number: digitArray.join(''), operator: '=' };
+    //    numberArray.push(digitArray.join(''));
+    numberArray.push(numberObj)
+    // Sorts the operations with pemdas
+    numberArray.forEach(numberObj => {
+        numberObj.operator === '=' ? numberObj.weight = 5 :
+            numberObj.operator === '-' ? numberObj.weight = 4 :
+                numberObj.operator === '+' ? numberObj.weight = 3 :
+                    numberObj.operator === '/' ? numberObj.weight = 2 :
+                        numberObj.operator === '*' ? numberObj.weight = 1 : alert('Error')
+    });
+    console.table(numberArray)
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < numberArray.length - 1; i++) {
+            if (numberArray[i]['weight'] > numberArray[i + 1]['weight']) {
+                let temp = numberArray[i];
+                numberArray[i] = numberArray[i + 1];
+                numberArray[i + 1] = temp;
+                swapped = true;
+            }
+            //console.table(numberArray)
+        }
+    } while (swapped);
+    console.table(numberArray)
+    digitArray = [];
+    operationString = numberArray.join(' ');
+
+});
--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
-const currentYear = new Date().getFullYear();
-document.querySelector('footer').textContent = `\u00A9 ${currentYear} | Alexander Eide`
+const currentDate = new Date();
+document.querySelector('footer').textContent = `\u00A9 ${currentDate.getFullYear()} | Alexander Eide`
+
+function getAge(dateString) {
+    let today = new Date();
+    let birthDate = new Date(dateString);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    let m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
+document.getElementById('myAge').textContent = `${getAge('March 31, 2002')}`;
